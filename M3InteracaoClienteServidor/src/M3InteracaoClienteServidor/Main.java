a8c4f643ce489936a66f2c7afbd300cbb38f046e
beboantonio <beboantonio1@hotmail.com>
2021-11-26 20:53:39 -0300

    adiciona a classe servidor

--- M3InteracaoClienteServidor/src/M3InteracaoClienteServidor/Main.java
+++ M3InteracaoClienteServidor/src/M3InteracaoClienteServidor/Main.java
@@ -2,7 +2,10 @@ package M3InteracaoClienteServidor;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world.");
+    public static void main(String[] args)
+    {
+        Server server = new Server();
+        server.runServer();
+
     }
 }
